d1e1ddb7deebbd438a559b74caffdc7150b6bc64
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-02-24 12:36:32 +0900

    $KOKODEIKKU_CHANNEL limits channels to read haiku

--- lib/ruboty/handlers/kokodeikku.rb
+++ lib/ruboty/handlers/kokodeikku.rb
@@ -4,6 +4,8 @@ require "ruboty"
 module Ruboty
   module Handlers
     class Kokodeikku < Base
+      env 'KOKODEIKKU_CHANNEL', "if specified, kokodeikku will be enabled only on the specified channels (separated by comma)", optional: true
+
       PREFIX = "ここで一句 "
 
       on(
@@ -14,12 +16,22 @@ module Ruboty
       )
 
       def kokodeikku(message)
+        return if channels && !channels.include?(message.to)
+
         if message.from != robot.name && !message.body.start_with?(PREFIX) && (song = reviewer.find(message.body))
           message.reply("#{PREFIX}#{song.phrases.map(&:join).join(' ')}")
         end
       end
 
       private
+
+      def channels
+        if ENV['KOKODEIKKU_CHANNEL']
+          @channels ||= ENV['KOKODEIKKU_CHANNEL'].split(',')
+        else
+          nil
+        end
+      end
 
       def reviewer
         @reviewer ||= Ikku::Reviewer.new
